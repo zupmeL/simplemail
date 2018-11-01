3325b9f9b80400b668598d2781219d4ef953a955
Stefano Prudenzi <stefano.prudenzi@gmail.com>
2018-11-01 12:58:05 +0100

    Add mongoose as dependency

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-const app = require ('./src/app');
+const app = require( './src/app' );
+const mongoose = require( 'mongoose' );
+
+mongoose.connect( process.env.MONGO_URI || "mongodb://0.0.0.0:27017/simplemail", {
+    useNewUrlParser: true
+} )
 
 app.listen(9999);
